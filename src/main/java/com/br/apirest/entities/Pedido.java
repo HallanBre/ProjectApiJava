1d3c6e4614c9fe67f5519b2693f97a5d24da22d5
HallanBre <halllanbandrad@gmail.com>
2023-09-01 19:45:17 -0300

    test

--- src/main/java/com/br/apirest/entities/Pedido.java
+++ src/main/java/com/br/apirest/entities/Pedido.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Pedido {
@@ -15,6 +17,10 @@ public class Pedido {
     private int id;
     private int data;
 
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+    
     //construtor
 
     public Pedido(int id, int data) {
--- src/main/java/com/br/apirest/entities/Usuario.java
+++ src/main/java/com/br/apirest/entities/Usuario.java
@@ -1,9 +1,13 @@
 package com.br.apirest.entities;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 
 @Entity
@@ -19,7 +23,8 @@ public class Usuario {
     private String email;
 
     
-
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+    private List<Pedido> pedidos;
 
 
 
