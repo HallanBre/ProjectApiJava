c8077777f6b985dc01b0ade3d73c27bc2ccf019b
Eduarda Manenti <eduarda_manenti@nuntec.com.br>
2023-09-01 23:56:27 -0300

    Adicionado o Lombok nas entidades.

--- src/main/java/com/br/apirest/entities/Pedido.java
+++ src/main/java/com/br/apirest/entities/Pedido.java
@@ -6,44 +6,23 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pedido {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private int data;
 
-    //fazer anotation do id
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private int data;
+	@ManyToOne
+	@JoinColumn(name = "usuario_id")
+	private Usuario usuario;
 
-    @ManyToOne
-    @JoinColumn(name = "usuario_id")
-    private Usuario usuario;
-    
-    //construtor
-
-    public Pedido(int id, int data) {
-        this.id = id;
-        this.data = data;
-    }
-
-    public Pedido() {
-        
-    }
-
-    //getters and setters
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public int getData() {
-        return data;
-    }
-    public void setData(int data) {
-        this.data = data;
-    }
-    
 }
--- src/main/java/com/br/apirest/entities/Usuario.java
+++ src/main/java/com/br/apirest/entities/Usuario.java
@@ -8,70 +8,25 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nome;
+	private String username;
+	private String cpf;
+	private String email;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String nome;
-    private String username;
-    private String cpf;
-    private String email;
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+	private List<Pedido> pedidos;
 
-    
-    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
-    private List<Pedido> pedidos;
-
-
-
-
-
-    public Usuario(int id, String nome, String username, String cpf, String email) {
-        this.id = id;
-        this.nome = nome;
-        this.username = username;
-        this.cpf = cpf;
-        this.email = email;
-    }
-    public Usuario(){
-
-    }
-    //Getters And setters 
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public String getCpf() {
-        return cpf;
-    }
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-    
 }
