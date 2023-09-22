3cfb6c3776a1f5f76d314bd34ae33105efeb5065
HallanBre <halllanbandrad@gmail.com>
2023-09-22 19:44:00 -0300

    service

---
+++ src/main/java/com/br/apirest/services/UsuarioService.java
@@ -0,0 +1,20 @@
+package com.br.apirest.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.br.apirest.entities.Usuario;
+import com.br.apirest.repository.UsuarioRepository;
+
+@Service //porq e uma classe de servico
+public class UsuarioService {
+    
+    @Autowired
+    UsuarioRepository repository;
+    public List<Usuario> listaUsuario(){
+        
+    }
+    
+}
